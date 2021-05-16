f99b4f87137b81fc4b85849062c16bf875289cc4
Sergey <markusanon5999@gmail.com>
2021-05-16 08:42:31 -0400

    Updates

--- lr3/script.sh
+++ lr3/script.sh
@@ -5,13 +5,16 @@ if [[ $find_directory ]];
 then
 source ./venv/bin/activate
 echo "Enrivonment python found"
+echo "****"
 else
 echo "Environment python not found"
 fi;
 if [[ $find_files ]];
 then
 chmod +x start.sh
+echo "*****"
 ./start.sh
+echo "*****"
 else
 echo "File start.sh not found"
 fi;
